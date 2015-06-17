4f232e769f8a300e35224044ae9b04fc683b92f6
eholzbach <ericholzbach@gmail.com>
2015-06-16 20:57:34 -0700

    added excludes and time options

--- manifests/periodic.pp
+++ manifests/periodic.pp
@@ -29,24 +29,32 @@
 #   Offset (in hours) when to run the cleanup job. (Default: `1`)
 #
 define tarsnap::periodic (
-  $ensure = present,
-  $dirs   = [],
-  $keep   = 30,
-  $hour   = fqdn_rand(24, $title),
-  $minute = fqdn_rand(60, $title),
-  $offset = 1,
+  $ensure   = present,
+  $dirs     = [],
+  $exclude  = [],
+  $keep     = 30,
+  $hour     = fqdn_rand(24, $title),
+  $minute   = fqdn_rand(60, $title),
+  $month    = undef,
+  $monthday = undef,
+  $offset   = 1,
+  $weekday  = undef,
 ) {
 
   validate_re($ensure, '^(present|absent)$')
 
   $dir_string = join($dirs, ' ')
+  $exc_string = prefix($exclude, ' --exclude ')
 
   cron { "tarsnap-${title}-create":
-    ensure  => $ensure,
-    command => "${::tarsnap::archive_path} ${title} ${dir_string}",
-    user    => $::tarsnap::user,
-    hour    => $hour,
-    minute  => $minute,
+    ensure   => $ensure,
+    command  => "${::tarsnap::archive_path} ${title} ${exc_string} ${dir_string}",
+    user     => $::tarsnap::user,
+    hour     => $hour,
+    minute   => $minute,
+    month    => $month,
+    monthday => $monthday,
+    weekday  => $weekday,
   }
 
   # what this does:
@@ -59,11 +67,14 @@ define tarsnap::periodic (
       ensure  => absent,
     }
     cron { "tarsnap-${title}-keep":
-      ensure  => $ensure,
-      command => "${::tarsnap::rotate_path} ${title} ${keep}",
-      user    => $::tarsnap::user,
-      hour    => $off_hour,
-      minute  => $minute,
+      ensure   => $ensure,
+      command  => "${::tarsnap::rotate_path} ${title} ${keep}",
+      user     => $::tarsnap::user,
+      hour     => $off_hour,
+      minute   => $minute,
+      month    => $month,
+      monthday => $monthday,
+      weekday  => $weekday,
     }
   }
 }
